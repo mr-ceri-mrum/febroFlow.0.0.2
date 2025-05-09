25616b6214059ffefde7f125a73d8891df4c2c67
Ilyas <79158764+mr-ceri-mrum@users.noreply.github.com>
2025-05-09 18:43:01 +0500

    Implement DalServiceRegistrations

---
+++ ServiceRegistrations/DalServiceRegistrations.cs
@@ -0,0 +1,34 @@
+using febroFlow.DataAccess.DataAccess;
+using febroFlow.DataAccess.DataAccess.Implementations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace febroFlow.Business.ServiceRegistrations;
+
+public static class DalServiceRegistrations
+{
+    public static IServiceCollection AddDalServices
+        (this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+    {
+        // Register Flow DAL
+        services.AddScoped<IFlowDal, FlowDal>();
+        
+        // Register Node DAL
+        services.AddScoped<INodeDal, NodeDal>();
+        
+        // Register Connection DAL
+        services.AddScoped<IConnectionDal, ConnectionDal>();
+        
+        // Register NodeType DAL
+        services.AddScoped<INodeTypeDal, NodeTypeDal>();
+        
+        // Register Webhook DAL
+        services.AddScoped<IWebhookDal, WebhookDal>();
+        
+        // Register Execution History DAL
+        services.AddScoped<IExecutionHistoryDal, ExecutionHistoryDal>();
+        
+        return services;
+    }
+}
