bf0337eb32bec2cd073c55e3ae72fc12a1853ee1
Ilyas <79158764+mr-ceri-mrum@users.noreply.github.com>
2025-05-09 18:42:50 +0500

    Implement CoreServiceRegistrations

---
+++ ServiceRegistrations/CoreServiceRegistrations.cs
@@ -0,0 +1,67 @@
+using febroFlow.Business.Services;
+using febroFlow.Business.Services.Implementations;
+using febroFlow.Core.Responses;
+using febroFlow.Core.Settings;
+using febroFlow.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace febroFlow.Business.ServiceRegistrations;
+
+public static class CoreServiceRegistrations
+{
+    public static IServiceCollection AddCoreServices
+        (this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+    {
+        #region DbContexts
+        var conn = "DefaultConnection";
+        services.AddDbContext<FlowDbContext>(options =>
+        {
+            options.UseNpgsql(
+                configuration.GetConnectionString(conn),
+                b => b.MigrationsAssembly("febroFlow.API")
+            );
+        });
+        #endregion
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddAuthorization();
+
+        // Register message repository
+        services.AddTransient<IMessagesRepository, MessagesRepository>();
+
+        // Register OpenAI service
+        services.Configure<OpenAISettings>(configuration.GetSection("OpenAI"));
+        services.AddScoped<IOpenAIService, OpenAIService>();
+
+        // Register Telegram service
+        services.Configure<TelegramSettings>(configuration.GetSection("Telegram"));
+        services.AddScoped<ITelegramService, TelegramService>();
+
+        // Register Flow services
+        services.AddScoped<INodeFactory, NodeFactory>();
+        services.AddScoped<IExecutionStateManager, ExecutionStateManager>();
+
+        #region Cors
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: configuration.GetSection("CorsLabel").Value!,
+                builder =>
+                {
+                    builder.WithMethods(
+                        configuration.GetSection("Methods").GetChildren().Select(x => x.Value).ToArray()!);
+                    builder.AllowAnyHeader();
+                    builder.AllowCredentials();
+                    builder.WithOrigins(
+                        configuration.GetSection("Origins").GetChildren().Select(x => x.Value).ToArray()!);
+                    builder.Build();
+                });
+        });
+        #endregion
+
+        return services;
+    }
+}
