4d138ae670d30b99c0ab6878affbb2333c208052
Ilyas Zhunussov <izhunussov@dmcorp.kz>
2025-05-13 16:31:43 +0500

    d

--- FebroFlow.Business/ServiceRegistrations/MediatrServiceRegistrations.cs
+++ FebroFlow.Business/ServiceRegistrations/MediatrServiceRegistrations.cs
@@ -11,35 +11,16 @@ namespace FebroFlow.Business.ServiceRegistrations
         public static IServiceCollection AddMediatrServices(
             this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
         {
-            #region Flow
-
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(FlowCreateCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(FlowUpdateCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(FlowDeleteCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(FlowExecuteCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(FlowTestCommand).Assembly));
-            #endregion
-        
-            #region Node
-
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(NodeCreateCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(NodeGetCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(NodeUpdateCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(NodeDeleteCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(NodeGetTypesCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(NodeGetByFlowCommand).Assembly));
-
+            // Регистрируем MediatR только один раз
+            services.AddMediatR(cnf => {
+                cnf.RegisterServicesFromAssemblies(typeof(FlowCreateCommand).Assembly);
+                cnf.RegisterServicesFromAssemblies(typeof(FlowTestCommand).Assembly);
+                cnf.RegisterServicesFromAssemblies(typeof(NodeCreateCommand).Assembly);
+                
+            });
             
-            #endregion
-
-            #region Con
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(ConnectionCreateCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(ConnectionDeleteCommand).Assembly));
-            services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(typeof(ConnectionUpdateCommand).Assembly));
-    
+            // Удаляем все повторные вызовы, которые вызывают ошибку
             
-
-            #endregion
             return services;
         }
     }
