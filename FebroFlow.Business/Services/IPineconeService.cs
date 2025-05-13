7ceecee66592cd8a6766d22cf025d8edc5bcf3b6
Ilyas Zhunussov <izhunussov@dmcorp.kz>
2025-05-13 16:04:38 +0500

    d

--- FebroFlow.Business/Services/IPineconeService.cs
+++ FebroFlow.Business/Services/IPineconeService.cs
@@ -67,7 +67,7 @@ public interface IPineconeService
 public class PineconeService(OpenAIClient openAiClient, IConfiguration configuration) : IPineconeService
 {
     private const string endpoint = "https://api.openai.com/v1/embeddings";
-    private const string model = "text-embedding-3-small";
+    private const string model = "";
     private const string apiKey = ""; // <-- вставьте ваш ключ
 
     
