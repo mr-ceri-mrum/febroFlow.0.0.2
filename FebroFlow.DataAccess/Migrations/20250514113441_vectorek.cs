bf005398e40e595156784fb7bb90dcaed917766e
Ilyas Zhunussov <izhunussov@dmcorp.kz>
2025-05-20 17:12:12 +0500

    added some think

---
+++ FebroFlow.DataAccess/Migrations/20250514113441_vectorek.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FebroFlow.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class vectorek : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Credentials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Data = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credentials", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flows",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SysteamPromt = table.Column<string>(type: "text", nullable: false),
+                    VectorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Tags = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flows", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatMemories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ChatId = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    FlowId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatMemories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChatMemories_Flows_FlowId",
+                        column: x => x.FlowId,
+                        principalTable: "Flows",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Parameters = table.Column<string>(type: "text", nullable: false),
+                    PositionX = table.Column<int>(type: "integer", nullable: false),
+                    PositionY = table.Column<int>(type: "integer", nullable: false),
+                    FlowId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CredentialId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DisableRetryOnFail = table.Column<bool>(type: "boolean", nullable: false),
+                    AlwaysOutputData = table.Column<bool>(type: "boolean", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Nodes_Flows_FlowId",
+                        column: x => x.FlowId,
+                        principalTable: "Flows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SourceNodeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TargetNodeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    SourceOutputIndex = table.Column<int>(type: "integer", nullable: true),
+                    TargetInputIndex = table.Column<int>(type: "integer", nullable: true),
+                    FlowId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Connections_Flows_FlowId",
+                        column: x => x.FlowId,
+                        principalTable: "Flows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Connections_Nodes_SourceNodeId",
+                        column: x => x.SourceNodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Connections_Nodes_TargetNodeId",
+                        column: x => x.TargetNodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExecutionStates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FlowId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CurrentNodeId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ContextId = table.Column<string>(type: "text", nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    FinishedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    InputData = table.Column<string>(type: "text", nullable: false),
+                    OutputData = table.Column<string>(type: "text", nullable: false),
+                    CurrentData = table.Column<string>(type: "text", nullable: false),
+                    ExecutionPath = table.Column<string>(type: "text", nullable: false),
+                    ErrorMessage = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExecutionStates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExecutionStates_Flows_FlowId",
+                        column: x => x.FlowId,
+                        principalTable: "Flows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExecutionStates_Nodes_CurrentNodeId",
+                        column: x => x.CurrentNodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMemories_FlowId",
+                table: "ChatMemories",
+                column: "FlowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_FlowId",
+                table: "Connections",
+                column: "FlowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_SourceNodeId",
+                table: "Connections",
+                column: "SourceNodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_TargetNodeId",
+                table: "Connections",
+                column: "TargetNodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExecutionStates_CurrentNodeId",
+                table: "ExecutionStates",
+                column: "CurrentNodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExecutionStates_FlowId",
+                table: "ExecutionStates",
+                column: "FlowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_FlowId",
+                table: "Nodes",
+                column: "FlowId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatMemories");
+
+            migrationBuilder.DropTable(
+                name: "Connections");
+
+            migrationBuilder.DropTable(
+                name: "Credentials");
+
+            migrationBuilder.DropTable(
+                name: "ExecutionStates");
+
+            migrationBuilder.DropTable(
+                name: "Nodes");
+
+            migrationBuilder.DropTable(
+                name: "Flows");
+        }
+    }
+}
---
+++ FebroFlow.DataAccess/Migrations/20250514113601_vectore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FebroFlow.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class vectore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vectors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IndexName = table.Column<string>(type: "text", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrganizationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vectors", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vectors");
+        }
+    }
+}
